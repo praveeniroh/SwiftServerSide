516e397d7a288b7a19360af1d2752d92834ac4de
iroh <prav7598@gmail.com>
2025-02-16 20:36:29 +0530

    PUT and DELETE operation API added in server

--- VaporServer/Sources/App/Controllers/SongController.swift
+++ VaporServer/Sources/App/Controllers/SongController.swift
@@ -13,6 +13,10 @@ struct SongController : RouteCollection{
         let songs = routes.grouped("songs")
         songs.get(use: index)
         songs.post(use: create)
+        songs.put(use: update)
+        songs.group(":id"){song in
+            song.delete(use: delete(request:))
+        }
     }
 
     @Sendable
@@ -25,5 +29,27 @@ struct SongController : RouteCollection{
         let song = try request.content.decode(Song.self)
         return song.create(on: request.db)
             .transform(to: .ok) //Returning HTTTP 200
+    }
+
+    @Sendable
+    private func update(request:Request) throws -> EventLoopFuture<HTTPStatus> {
+        let song = try request.content.decode(Song.self)
+        return Song.find(song.id, on: request.db)
+            .unwrap(or: Abort(.notFound))
+            .flatMap{
+                $0.title = song.title
+                return $0.update(on: request.db).transform(to: .ok)
+            }
+    }
+
+    ///DELETE songs/<song id>
+    @Sendable
+    private func delete(request:Request) throws -> EventLoopFuture<HTTPStatus> {
+        return Song.find(request.parameters.get("id"), on: request.db)
+            .unwrap(or: Abort(.notFound))
+            .flatMap{
+                $0.delete(on: request.db).transform(to: .ok)
+            }
+
     }
 }
